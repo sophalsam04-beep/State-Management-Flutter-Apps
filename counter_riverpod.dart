aeb223e4180790163d1e8b13c6758576f8b490fa
Phal Developer <sophalsam04@gmail.com>
2026-08-06 22:56:23 +0700

    Create counter_riverpod.dart

---
+++ counter_riverpod.dart
@@ -0,0 +1,5 @@
+  // create a Provider Counter
+import 'package:flutter_riverpod/legacy.dart';
+  // using to Stateprovider to store in Provider
+
+      final counterProvider = StateProvider<int>((ref) => 0);
